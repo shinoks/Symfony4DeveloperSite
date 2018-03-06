224eda89b977e3ffc23655454100f718bd6b0657
Shinoks <shinoks@o2.pl>
2018-03-06 13:07:45 +0100

    Menu Service order

--- src/Service/Menu.php
+++ src/Service/Menu.php
@@ -14,7 +14,7 @@ class Menu
     }
 
     public function getMenu() {
-        $menu = $this->em->getRepository(Men::class)->findBy(['isActive' => 1, 'parent' => null],['position' => 'ASC', 'id' => 'ASC']);
+        $menu = $this->em->getRepository(Men::class)->findBy(['isActive' => 1, 'parent' => null],['position' => 'ASC', 'id' => 'DESC']);
 
         return $menu;
     }
