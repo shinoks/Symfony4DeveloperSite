d15399c469e44f87d0b3e2895d9058698624f73f
Shinoks <shinoks@o2.pl>
2018-03-16 10:50:56 +0100

    Zip Code added, Pesel validator in js

---
+++ src/Controller/ZipCodeController.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\ZipCode;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\CommentType;
+
+class ZipCodeController extends Controller
+{
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
+    /**
+     * @return Response
+     */
+    public function index()
+    {
+        $zipCodes = $this->getDoctrine()
+            ->getRepository(ZipCode::class)
+            ->findAll();
+
+        return $this->render('front/articles.html.twig',array(
+            'zipCodes'=> $zipCodes
+        ));
+    }
+
+    /**
+     * @return Response
+     */
+    public function show($id,Request $request)
+    {
+        $article = $this->getDoctrine()
+            ->getRepository(ZipCode::class)
+            ->find($id);
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->session->getFlashBag()->add('success', 'Komentarz został́ dodany');
+        }
+        return $this->render('front/article_show.html.twig',array(
+            'article'=> $article
+        ));
+    }
+
+    /**
+     * @return Response
+     */
+    public function showByCategory($categoryId)
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $categoryId, 'isActive' => 1],['created' => 'DESC']);
+
+        return $this->render('front/articles.html.twig',array(
+            'articles'=> $articles
+        ));
+    }
+
+    /**
+     * @return Response
+     */
+    public function getZipCode($zipCode)
+    {
+        $zipCode = $this->getDoctrine()
+            ->getRepository(ZipCode::class)
+            ->findOneBy(['code' => $zipCode]);
+
+        $jsonData = ['code' => $zipCode->getCode(), 'city' => $zipCode->getCity(), 'state' => $zipCode->getState()];
+
+        return new JsonResponse($jsonData);
+    }
+
+}
