af0266284536b6cde7bfbde8373edabdfd0ac075
Shinoks <shinoks@o2.pl>
2018-06-06 10:19:46 +0200

    Phone added to register user

--- src/Form/UserDataType.php
+++ src/Form/UserDataType.php
@@ -2,17 +2,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserDataType extends AbstractType
@@ -20,7 +13,7 @@ class UserDataType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('phone',TelType::class,['label'=>'phone'])
+        ->add('phone',TelType::class,['label'=>'phone','required' => true])
         ->add('pesel',NumberType::class,['label'=>'pesel'])
         ->add('zipCode',TextType::class,['label'=>'zip_code'])
         ->add('address',TextType::class,['label'=>'address'])
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -15,10 +16,11 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('email',TextType::class,['label'=>'first_name'])
-        ->add('password',PasswordType::class,['label'=>'password.password'])
-        ->add('firstName',TextType::class,['label'=>'first_name'])
-        ->add('lastName',TextType::class,['label'=>'last_name'])
+        ->add('email',TextType::class,['label'=>'first_name','required' => true])
+        ->add('password',PasswordType::class,['label'=>'password.password','required' => true])
+        ->add('firstName',TextType::class,['label'=>'first_name','required' => true])
+        ->add('lastName',TextType::class,['label'=>'last_name','required' => true])
+        ->add('phone',TelType::class,['label'=>'phone','required' => true])
         ->add('gender',ChoiceType::class, [
                 'label'=>'gender',
                 'multiple' => false,
@@ -26,10 +28,13 @@ class UserType extends AbstractType
                 'choices' => [
                     'Pani' => 'f',
                     'Pan' => 'm'
-                ]])
+                ],
+                'required' => true
+        ])
         ->add('birthDate',BirthdayType::class,[
             'label'=>'birth_date',
-            'placeholder' => 'Wybierz datę'
+            'placeholder' => 'Wybierz datę',
+            'required' => true
         ])
         ->add('regulations',CheckboxType::class, ['label'=>'regulations','required' => true ])
         ->add('marketingRegulations',CheckboxType::class, ['label'=>'marketing_regulations','required' => false ])
